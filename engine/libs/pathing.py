aaeed92461c952809f3bbe92c36f438d76b36cdb
Teifion <sarkalian@gmail.com>
2011-07-31 16:50:32 +0100

    Added placeholder for some pathing functions

---
+++ engine/libs/pathing.py
@@ -0,0 +1,12 @@
+import multiprocessing
+
+def quick_path(start, finish, terrain, actors, actor_size=1):
+    """Used for a path that is expected to be easy to compute
+    or one that you need a result for right now."""
+    pass
+    
+
+def threaded_path():
+    """Used for paths that you do not need right away, this function
+    uses multiprocessing so that you can compute the path in the background"""
+    pass
