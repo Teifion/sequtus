c327af12ce95eb9bd718c3ff97c283571544f5a3
Teifion <sarkalian@gmail.com>
2011-10-02 21:54:57 +0100

    Abilities now have an effect_offset that allows the create the effect at a specific point

--- engine/logic/abilities.py
+++ engine/logic/abilities.py
@@ -30,7 +30,7 @@ class Ability (object):
     ignore_z_facing = False
     
     # The location relative to our host that we draw from
-    origin_offset = [0,0]
+    effect_offset = [0,0,0]
     
     # These are used by the system as it's easier to calculate
     # using these when actors are rotated etc but it's easier
@@ -39,6 +39,9 @@ class Ability (object):
     _offset_distance = 0
     _offset_angle = 0
     
+    image = None
+    image_offset = [0,0,0]
+    
     def __init__(self, actor, ability_data={}):
         super(Ability, self).__init__()
         self.actor = actor
@@ -46,6 +49,9 @@ class Ability (object):
         
         self.set_stats(ability_data)
         self.charge = self.required_charge
+        
+        self._offset_distance = vectors.distance(self.effect_offset)
+        self._offset_angle = vectors.angle(self.effect_offset)
     
     def set_stats(self, ability_data):
         for k, v in ability_data.items():
@@ -245,7 +251,7 @@ class RepairAbility (Ability):
         colour = [self.effect['colour'][i] + random.random() * self.effect['variation'][i] for i in range(3)]
         
         the_effect = effects.Beam(
-            origin=self.actor.pos,
+            origin=vectors.add_vectors(self.actor.pos, self.effect_offset),
             target=target.pos,
             colour=effects.bound_colour(colour),
             duration=self.required_charge-1,
@@ -282,18 +288,29 @@ class ProjectileWeapon (WeaponAbility):
 
 class MassDriver (ProjectileWeapon):
     def generate_bullet(self, target):
+        # Set correct origin
+        offset_angle = vectors.bound_angle(
+            vectors.add_vectors(self._offset_angle, self.facing)
+        )
+        
+        origin_pos = vectors.add_vectors(
+            self.actor.pos,
+            vectors.move_to_vector(offset_angle, self._offset_distance)
+        )
+        
+        # Get actual velocity we'll be using
         if type(target) == list or type(target) == tuple:
-            direction = vectors.angle(self.actor.pos, target)
+            direction = vectors.angle(origin_pos, target)
             target_pos = target
         else:
-            direction = vectors.angle(self.actor.pos, target.pos)
+            direction = vectors.angle(origin_pos, target.pos)
             target_pos = target.pos
         
         velocity = vectors.move_to_vector(direction, self.bullet['velocity'])
         velocity[2] = math_lib.calc_trajectory(0.1, vectors.distance(self.actor.pos, target_pos), self.bullet['velocity'])
         
         the_bullet = bullets.Shell(
-            pos=self.actor.pos,
+            pos=origin_pos,
             velocity=velocity,
             image = self.bullet['image'],
             size = self.bullet['size'],
@@ -305,8 +322,17 @@ class MassDriver (ProjectileWeapon):
 
 class BeamWeapon (InstantHitWeapon):
     def generate_effect(self, target):
+        offset_angle = vectors.bound_angle(
+            vectors.add_vectors(self._offset_angle, self.facing)
+        )
+        
+        origin_pos = vectors.add_vectors(
+            self.actor.pos,
+            vectors.move_to_vector(offset_angle, self._offset_distance)
+        )
+        
         the_effect = effects.Beam(
-            origin=self.actor.pos,
+            origin=origin_pos,
             target=target.pos,
             colour=self.effect['colour'],
             duration=self.effect['duration'],
