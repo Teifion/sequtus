e64d9bbe0707bff37f650c040f514a3331958529
Teifion <sarkalian@gmail.com>
2011-10-07 23:35:22 +0100

    Altered code to test animation (specifically rotations)

--- game/seq_game.py
+++ game/seq_game.py
@@ -139,7 +139,9 @@ class Sequtus (core.EngineV3):
         # Wrap it in a try block so that the screen can quit it's AI processes
         try:
             # Issue an attack order
-            self.current_screen.select_actor(self.current_screen.actors[0])
+            # self.current_screen.select_actor(self.current_screen.actors[0])
+            self.current_screen.add_order(self.current_screen.actors[0], "move", pos=[120, 120, 0], target=None)
+            self.current_screen.queue_order(self.current_screen.actors[0], "move", pos=[500, 120, 0], target=None)
 
             # S key to issue stop command
             # e = pygame.event.Event(3, scancode=2, key=100, mod=0)
