b8f4a9fcf3eb5f180d396c5af7380bae84584e99
Teifion <sarkalian@gmail.com>
2011-10-09 16:32:01 +0100

    Added crude base building logic

--- engine/ai/basic_computer_ai.py
+++ engine/ai/basic_computer_ai.py
@@ -3,6 +3,7 @@ import traceback
 import sys
 
 from engine.ai import core_ai
+from engine.libs import actor_lib, vectors
 
 line_match = re.compile(r"(([0-9])\* )?(.*)")
 
@@ -30,8 +31,6 @@ class BasicComputerAI (core_ai.AICore):
                 except Exception as e:
                     traceback.print_exc(file=sys.stdout)
                     print("Error loading bases, abandoning loading of further bases")
-        
-        print(self.bases)
     
     def _save_base(self, base_name, base_data):
         base_setup = {
@@ -57,12 +56,59 @@ class BasicComputerAI (core_ai.AICore):
         self.bases[base_name] = base_setup
     
     def inspect_bases(self):
+        buildings_missing = []
+        
+        # First find out which buildings we are missing
         for base_name, base_data in self.bases.items():
-            for b in base_data['buildings']:
-                pass
+            for building_type, mapping in base_data['buildings']:
+                # If we can't find the mapping in our own actors then set it to none
+                if mapping not in self.own_actors:
+                    mapping = None
+                
+                buildings_missing.append((base_name, building_type))
+        
+        if buildings_missing == []:
+            return None
+        
+        # Now narrow down our list of builders so we don't later iterate over
+        # non-builders more than once
+        builders = []
+        builders_used = []
+        for aid, a in self.own_actors.items():
+            if self.actor_types[a.actor_type]['can_construct']:
+                if a.current_order[0] == "stop":
+                    builders.append(a)
+        
+        # Now we work out which buildings we can build and who is closest to it
+        buildings_we_can_build = []
+        for base, building in buildings_missing:
+            target_pos = self.bases[base]['location']
+            
+            closest_builder = None, 9999999
+            for b in builders:
+                if b in closest_builder: continue
+                if actor_lib.can_build(
+                    self.actor_types[b.actor_type],
+                    self.actor_types[building],
+                    self.build_lists
+                ):
+                    # If they are closest we want them to go build it
+                    dist = vectors.distance(target_pos, b.pos)
+                  
+                    if dist < closest_builder[1]:
+                        closest_builder = b, dist
+            
+            # Now remove them from the pool and issue the build order
+            if closest_builder[0] != None:
+                builders_used.append(closest_builder[0])
+                self.issue_orders(closest_builder[0].oid, cmd="build", pos=target_pos, target=building)
+            
+            
             
     
     def cycle(self):
+        self.inspect_bases()
+        
         for k in self.enemy_actors.keys(): first_enemy = k
         
         if self.actors_updated:
