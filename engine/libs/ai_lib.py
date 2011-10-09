3982e77b93771bd8f8477d6e49be243cdecbeac7
Teifion <sarkalian@gmail.com>
2011-10-09 20:44:00 +0100

    Added function for placement of buildings

--- engine/libs/ai_lib.py
+++ engine/libs/ai_lib.py
@@ -1,3 +1,4 @@
+from engine.libs import sim_lib
 
 def send_static_data(screen, queue):
     """Used to send data such as the templates of units, stuff you only need to send once"""
@@ -17,4 +18,31 @@ def send_static_data(screen, queue):
     # Send build lists
     queue.put({"cmd":"build_lists", "build_lists":dict(screen.build_lists)})
 
+def place_actor(actor_list, building_rect, distance=100):
+    """Tests several positions for the building by nudging it around. This is
+    because an AI may not always have the most recent positions for actors."""
+    
+    ox, oy = building_rect.left, building_rect.right
+    
+    nudges = (
+        (0,0),
+        
+        (-1,-1),
+        (-1,0),
+        (-1,1),
+        (0,-1),
+        (0,1),
+        (1,-1),
+        (1,0),
+        (1,1),
+    )
+    
+    for x,y in nudges:
+        building_rect.left = ox + x * distance
+        building_rect.top = oy + y * distance
+        
+        if not sim_lib.test_possible_collision(actor_list, building_rect, True):
+            return building_rect
+    
+    return None
 
