e49efb48945211be11f6d8ab9cb795c355ea44ae
Teifion <sarkalian@gmail.com>
2011-10-09 23:37:46 +0100

    Updated the place_actor test

--- engine/test_lib/ai_lib_t.py
+++ engine/test_lib/ai_lib_t.py
@@ -7,25 +7,25 @@ class DummyActor (object):
         super(DummyActor, self).__init__()
         self.rect = pygame.Rect(*args)
 
-class MathTests(unittest.TestCase):
+class AILibTests(unittest.TestCase):
     def test_place_actor(self):
         vals = (
-            ([DummyActor(1000, 1000, 50, 50)], pygame.Rect(1000, 1000, 50, 50), pygame.Rect(900, 1000, 50, 50)),
+            ([DummyActor(1000, 1000, 50, 50)], pygame.Rect(1000, 1000, 50, 50), pygame.Rect(900, 950, 50, 50)),
             
             ([
                 DummyActor(1000, 1000, 50, 50),
                 DummyActor(900, 1000, 50, 50),
-            ], pygame.Rect(1000, 1000, 50, 50), pygame.Rect(1100, 1000, 50, 50)),
+            ], pygame.Rect(1000, 1000, 50, 50), pygame.Rect(900, 1150, 50, 50)),
             
             ([
                 DummyActor(1879, 1879, 41, 50),
                 DummyActor(1779, 1816, 41, 50),
                 DummyActor(1679, 1716, 41, 50),
                 DummyActor(1779, 1816, 41, 50),
-            ], pygame.Rect(1700, 1800, 50, 50), pygame.Rect(1700, 1800, 50, 50)),
+            ], pygame.Rect(1700, 1800, 50, 50), pygame.Rect(1600, 1650, 50, 50)),
         )
         
         for actor_list, new_rect, expected in vals:
-            self.assertEqual(expected, ai_lib.place_actor(actor_list, new_rect))
+            self.assertEqual(expected, ai_lib.place_actor(actor_list, new_rect, distance=100))
 
-suite = unittest.TestLoader().loadTestsFromTestCase(MathTests)
+suite = unittest.TestLoader().loadTestsFromTestCase(AILibTests)
