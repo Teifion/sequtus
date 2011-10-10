f70dd8f19a4e1533f7e5913113b6908930df582d
Teifion <sarkalian@gmail.com>
2011-10-11 00:06:51 +0100

    Refactored the AI base building logic, it no longer looks to map buildings but refreshes on the fly (probably slower but speed isn't an issue for the AI at all yet)

--- engine/ai/basic_computer_ai.py
+++ engine/ai/basic_computer_ai.py
@@ -60,48 +60,78 @@ class BasicComputerAI (core_ai.AICore):
         
         # First find out which buildings we are missing
         for base_name, base_data in self.bases.items():
-            for building_type, mapping in base_data['buildings']:
-                # If we can't find the mapping in our own actors then set it to none
-                if mapping not in self.own_actors:
-                    mapping = None
+            base_area = (
+                base_data['location'][0] - base_data['size'][0],
+                base_data['location'][1] - base_data['size'][1],
                 
-                buildings_missing.append((base_name, building_type))
-        
-        if buildings_missing == []:
-            return None
-        
-        # Now narrow down our list of builders so we don't later iterate over
-        # non-builders more than once
-        builders = []
-        builders_used = []
-        for aid, a in self.own_actors.items():
-            if self.actor_types[a.actor_type]['can_construct']:
-                if a.current_order[0] == "stop":
-                    builders.append(a)
-        
-        # Now we work out which buildings we can build and who is closest to it
-        buildings_we_can_build = []
-        for base, building in buildings_missing:
-            target_pos = self.bases[base]['location']
+                base_data['location'][0] + base_data['size'][0],
+                base_data['location'][1] + base_data['size'][1],
+            )
             
-            closest_builder = None, 9999999
-            for b in builders:
-                if b in closest_builder: continue
-                if actor_lib.can_build(
-                    self.actor_types[b.actor_type],
-                    self.actor_types[building],
-                    self.build_lists
-                ):
-                    # If they are closest we want them to go build it
-                    dist = vectors.distance(target_pos, b.pos)
-                  
-                    if dist < closest_builder[1]:
-                        closest_builder = b, dist
+            buildings_needed = set([b[0] for b in base_data['buildings']])
             
-            # Now remove them from the pool and issue the build order
-            if closest_builder[0] != None:
-                builders_used.append(closest_builder[0])
-                self.issue_orders(closest_builder[0].oid, cmd="build", pos=target_pos, target=building)
+            # Get all buildings within this base
+            found_buildings = {}
+            total_needed = {}
+            for b in buildings_needed:
+                found_buildings[b] = 0
+                total_needed[b] = 0
+            
+            for b in base_data['buildings']:
+                if b[1] == None:
+                    total_needed[b[0]] += 1
+            
+            # Loop through all actors and see what we've got
+            for i, a in self.own_actors.items():
+                if actor_lib.is_inside(a, base_area):
+                    if a.actor_type in buildings_needed:
+                        found_buildings[a.actor_type] += 1
+            
+            # Now find out what we are missing
+            missing = {}
+            for b in buildings_needed:
+                m = total_needed[b] - found_buildings[b]
+                if m > 0:
+                    missing[b] = m
+            
+            # None missing? We can stop looking around now
+            if missing == {}:
+                continue
+            
+            # Now find out which builders we can use
+            # Narrow down our list of builders so we don't later iterate over
+            # non-builders more than once
+            builders = []
+            builders_used = []
+            for aid, a in self.own_actors.items():
+                if self.actor_types[a.actor_type]['can_construct']:
+                    if a.current_order[0] == "stop":
+                        builders.append(a)
+            
+            # Now we work out which buildings we can build and who is closest to it
+            for building_type, amount in missing.items():
+                for i in range(amount):
+                    target_pos = base_data['location']
+                    
+                    closest_builder = None, 9999999
+                    for b in builders:
+                        if b in closest_builder: continue
+                        if actor_lib.can_build(
+                            self.actor_types[b.actor_type],
+                            self.actor_types[building_type],
+                            self.build_lists
+                        ):
+                            # If they are closest we want them to go build it
+                            dist = vectors.distance(target_pos, b.pos)
+                            
+                            if dist < closest_builder[1]:
+                                closest_builder = b, dist
+                    
+                    # Now remove them from the pool and issue the build order
+                    if closest_builder[0] != None:
+                        builders_used.append(closest_builder[0])
+                        self.issue_orders(closest_builder[0].oid, cmd="build", pos=target_pos, target=building_type)
+            
             
             
             
